e0514e3da1358f9429376795bb02f642ce57aa7b
Vale Guzmán <mvale888@gmail.com>
2023-01-29 22:34:43 -0300

    agregue branch deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mvale888', // Usually your GitHub org/user name.
   projectName: 'manual-SSD', // Usually your repo name.
-
+  deploymentBranch: 'deploy',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
